bcff0635cae42151b8142457f81f57720e1b72ab
rin0207 <rintaku.7@gmail.com>
2020-06-22 23:39:16 +0900

    [Update2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-     resource :favorites, only: [:create, :destroy]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   root :to => 'homes#top'
   get 'home/about' => 'homes#about'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
